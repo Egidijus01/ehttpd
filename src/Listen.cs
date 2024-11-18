a924631cf58a23521828c19cc13ff351b8683862
Egidijus01 <egidijusgagela@gmail.com>
2024-11-18 21:13:49 +0200

    implement normal socket and port binding

--- src/Listen.cs
+++ src/Listen.cs
@@ -1,18 +1,85 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using List;
+using MainStructures;
 
 namespace Listen
 {
 	public class Listener
 	{
-		public struct ListenerConfig {
-			public LinkedList.ListHead list;
-			public int socket;
-			public int n_clients;
+		public struct AddressInfo
+		{
+			public AddressFamily Family;
+			public SocketType SocketType;
+			public SocketFlags Flags;
 		}
 
-		public static int BindSocket(string host, string port) {
-			Console.WriteLine($"Binding socket to host: {host}, port: {port}");
-			return 1;
+		private struct ListenerConfig
+		{
+			public LinkedList.ListHead List;
+			public FileStream Sfd;
+			public IPEndPoint Addr;
+			public Socket Socket;
+			public int NClients;
+			public bool Blocked;
+			public bool Tls;
+		}
+
+		public static int BindSocket(string host, string port, bool tls)
+		{
+			int bound = 0;
+			Socket socket = null;
+			var hints = new AddressInfo
+			{
+				Family = AddressFamily.Unspecified,
+				SocketType = SocketType.Stream,
+				Flags = SocketFlags.None
+			};
+
+			try
+			{
+				var addresses = Dns.GetHostAddresses(host);
+				foreach (var address in addresses)
+				{
+					var endpoint = new IPEndPoint(address, int.Parse(port));
+
+					socket = new Socket(endpoint.AddressFamily, hints.SocketType, ProtocolType.Tcp);
+
+					socket.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+
+					if (endpoint.AddressFamily == AddressFamily.InterNetworkV6)
+					{
+						socket.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, true);
+					}
+
+					socket.Bind(endpoint);
+
+					socket.Listen(10);
+
+					ListenerConfig listener = new ListenerConfig
+					{
+						Socket = socket,
+						Addr = endpoint,
+						Tls = tls,
+						NClients = 0,
+						Blocked = false
+					};
+
+					bound++;
+				}
+
+				return bound;
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error binding socket: {ex.Message}");
+				return 0;
+			}
+			finally {
+				socket?.Close();
+			}
 		}
 	}
 }
